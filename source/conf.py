afa8b9862a4aa301a045335152fed07693de585a
Julie Schramm <julie.schramm@noaa.gov>
2019-10-23 13:49:29 -0600

    Try html_theme = 'sphinx_rtd_theme'

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -87,7 +88,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+#JLShtml_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
